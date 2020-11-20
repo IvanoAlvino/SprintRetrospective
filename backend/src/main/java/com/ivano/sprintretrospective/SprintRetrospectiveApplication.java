377e22e5dad643d2fb8342e118dc679356724cea
Ivano Alvino <ivano.alvino@navvis.com>
2020-11-20 12:07:21 +0100

    Initial frontend and backend commit

---
+++ backend/src/main/java/com/ivano/sprintretrospective/SprintRetrospectiveApplication.java
@@ -0,0 +1,14 @@
+package com.ivano.sprintretrospective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintRetrospectiveApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintRetrospectiveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ivano/sprintretrospective/SprintRetrospectiveApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ivano.sprintretrospective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintRetrospectiveApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
